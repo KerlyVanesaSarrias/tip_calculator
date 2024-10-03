06d18806a95b700648c021947d796b8137542375
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-03 16:27:08 -0500

    added component MenuItem and created a hook for the order useOrder

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import type { MenuItem, OrderItem } from "../types";
+
+
+
+const useOrder = () => {
+
+    const [order, setOrder] = useState<OrderItem[]>([]);
+
+    const addItem = (item: MenuItem) => {
+        const itemExist = order.find(OrderItem => OrderItem.id === item.id);
+        if(itemExist) {
+            const updateOrder = order.map(orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity +1 } : orderItem)
+                setOrder(updateOrder);
+        }else {
+            const newItem = {...item, quantity:1}
+            setOrder([...order, newItem]);
+        }
+
+    }
+    return {addItem}
+
+
+}
+
+export default useOrder
